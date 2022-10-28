ce437c84e921a2facdfe5454a13f9c3a03f49385
OlenaLotos <olenameshanych@gmail.com>
2022-10-28 22:57:43 +0300

    update

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,6 +1,7 @@
 const { User } = require("../../models");
 const path = require("path");
 const fs = require("fs/promises");
+// const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
@@ -10,7 +11,17 @@ const updateAvatar = async (req, res) => {
   const imageName = `${id}_${originalname}`;
   try {
     const resultUpload = path.join(avatarsDir, imageName);
+
+    // await Jimp.read(tempUpload)
+    //   .then((avatar) => {
+    //     return avatar.resize(250, 250).write(resultUpload);
+    //   })
+    //   .catch((err) => {
+    //     throw err;
+    //   });
+
     await fs.rename(tempUpload, resultUpload);
+
     const avatarURL = path.join("public", "avatars", imageName);
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
     res.json({ avatarURL });
